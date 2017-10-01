92999cc410c1264fe7a171174e286dc8ad4ee196
Rodrigo Soares Souza <rodsoadev@gmail.com>
2017-09-30 21:14:31 -0300

    subindo vendors do sistema

---
+++ vendor/jeroennoten/laravel-adminlte/src/Menu/Filters/GateFilter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace JeroenNoten\LaravelAdminLte\Menu\Filters;
+
+use Illuminate\Contracts\Auth\Access\Gate;
+use JeroenNoten\LaravelAdminLte\Menu\Builder;
+
+class GateFilter implements FilterInterface
+{
+    protected $gate;
+
+    public function __construct(Gate $gate)
+    {
+        $this->gate = $gate;
+    }
+
+    public function transform($item, Builder $builder)
+    {
+        if (! $this->isVisible($item)) {
+            return false;
+        }
+
+        return $item;
+    }
+
+    protected function isVisible($item)
+    {
+        return ! isset($item['can']) || $this->gate->allows($item['can']);
+    }
+}
